c8e08824f8e6a383ca6e374c430ad4d33b9d6b9f
justin.zhao <zhaojunhua@hisilicon.com>
2015-11-03 18:41:04 +0800

    Add kernel source code as new parameter
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

--- lamp/build.sh
+++ lamp/build.sh
@@ -4,12 +4,13 @@
 #description: just a example for build&install scripts
 #$1: target platform name
 #$2: target distributions name
-#$3: target rootfs directory
+#$3: target rootfs directory(absolutely)
+#$4: kernel source code directory(absolutely)
 #return: 0: build success, other: failed
 
-echo "Building armor ..."
-echo "Platform=$1, distro=$2,rootfs=$3."
+echo "Building package <your name> ..."
+echo "Platform=$1, distro=$2, rootfs=$3, kernel=$4"
 
-echo "Installing armor ..."
+echo "Installing package <your name> ..."
 
 exit 0
