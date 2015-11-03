fa20ac36fe85e9a65db3cbc4ad838b5ffa835ad0
justin.zhao <zhaojunhua@hisilicon.com>
2015-11-03 17:31:12 +0800

    Add docker
    Signed-off-by: justin.zhao <zhaojunhua@hisilicon.com>

---
+++ docker/build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#author: Justin Zhao
+#date: 31/10/2015
+#description: just a example for build&install scripts
+#$1: target platform name
+#$2: target distributions name
+#$3: target rootfs directory
+#return: 0: build success, other: failed
+
+echo "Building docker ..."
+echo "Platform=$1, distro=$2,rootfs=$3."
+
+echo "Installing docker ..."
+
+exit 0
---
+++ docker/docker_remove.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#author: Justin Zhao
+#date: 31/10/2015
+#description: just a example for remove scripts
