9cef9f9e5a0c124b68b4dee0f0c4eb0e40b3d025
Anurup M <anurup.m@huawei.com>
2015-11-06 19:52:18 +0530

    Add scripts for Armor tools build and install
    Signed-off-by: Anurup M <anurup.m@huawei.com>

--- armor/armor_postinstall.sh
+++ armor/armor_postinstall.sh
@@ -1,5 +1,83 @@
 #!/bin/bash
-#author: Justin Zhao
-#date: 31/10/2015
-#description: just a example for postinstall scripts
+#author: Shiju Jose
+#date: 05/11/2015
+#description: postinstall scripts for Armor Tools
+
+# Install Armor Tools packages
+# Uncomment below line to install Armor tool's packages.
+INSTALL_ARMOR_TOOLS="YES"
+if [ "$INSTALL_ARMOR_TOOLS" = 'YES' ]; then
+    Distribution=`cat /etc/issue| cut -d' ' -f 1`
+    echo "Installing Armor tools packages on $Distribution..." 
+
+    case "$Distribution" in
+        Ubuntu) 
+            #echo "Ubuntu Distribution"
+            apt-get install -y sysstat  # sar
+            apt-get install -y gdb  
+            apt-get install -y iptables  
+           
+            apt-get install -y dstat  
+            apt-get install -y iotop  
+            apt-get install -y blktrace  
+            apt-get install -y nicstat  
+            apt-get install -y libconfig9  
+            apt-get install -y lldpad  
+            apt-get install -y oprofile  
+            apt-get install -y latencytop 
+            apt-get install -y systemtap  
+            apt-get install -y crash 
+	    #Install perf tool
+	    apt-get install -y linux-tools-3.19.0-23 
+
+            # install prebuilt deb packages   
+            dpkg -i /usr/local/armor/binary/dmidecode-1.0-arm64.deb
+            dpkg -i /usr/local/armor/binary/tiptop-2.3_arm64.deb
+            ;;
+           
+        Fedora) 
+            #echo "Fedora Distribution"
+            dnf install -y sysstat.aarch64 # sar
+            dnf install -y dmidecode.aarch64
+            dnf install -y tcpdump.aarch64
+            dnf install -y ethtool.aarch64
+           
+            dnf install -y dstat
+            dnf install -y tiptop.aarch64
+            dnf install -y iotop.noarch
+            dnf install -y blktrace.aarch64
+            dnf install -y nicstat.aarch64
+            dnf install -y lldpad.aarch64
+            dnf install -y oprofile.aarch64
+            dnf install -y latencytop.aarch64
+            dnf install -y systemtap.aarch64
+            dnf install -y crash.aarch64
+
+            # install prebuilt rpm packages 
+            ;;
+         
+        OpenSuse) 
+            #echo "OpenSuse Distribution"
+            zypper install -y ltrace
+            zypper install -y pcp-import-iostat2pcp # for sar, iostat etc.
+            zypper install -y dmidecode
+            zypper install -y strace
+            zypper install -y net-tools-deprecated
+            zypper install -y net-tools
+            
+            zypper install -y dstat 
+            zypper install -y procps
+            zypper install -y iotop
+            zypper install -y blktrace
+            zypper install -y oprofile
+            zypper install -y systemtap
+            
+            # install prebuilt rpm packages 
+            rpm -i /usr/local/armor/binary/lldpad-1.0.1-0.aarch64.rpm 
+            rpm -i /usr/local/armor/binary/nicstat-1.95-0.aarch64.rpm 
+            rpm -i /usr/local/armor/binary/tiptop-2.3-0.aarch64.rpm
+            ;;
+        esac
+        echo "Finished installation of Armor tools packages" 
+    fi
 
--- armor/build.sh
+++ armor/build.sh
@@ -1,16 +1,112 @@
 #!/bin/bash
 #author: Justin Zhao
 #date: 31/10/2015
-#description: just a example for build&install scripts
+#author: Shiju Jose
+#date: 05/11/2015
+#description: Armor build & install script
 #$1: target platform name
 #$2: target distributions name
 #$3: target rootfs directory(absolutely)
 #$4: kernel build directory(absolutely)
 #return: 0: build success, other: failed
 
-echo "Building package <your name> ..."
-echo "Platform=$1, distro=$2, rootfs=$3, kernel=$4"
+###################################################################################
+###################### Initialise variables ####################
+###################################################################################
 
-echo "Installing package <your name> ..."
+echo "/packages/armor/build.sh: Platform=$1, distro=$2, rootfs=$3, kernel=$4"
+
+if [ "$1" = '' ] || [ "$2" = '' ] ||  [ "$3" = '' ]  || [ "$4" = '' ]; then
+    echo "Invalid parameter passed. Usage ./armor/build.sh <platform> <distrib> <rootfs> <kernal>" 
+    exit
+fi
+
+PLATFORM=$1
+DISTRO=$2
+ROOTFS=$3
+
+armor_dir=armor
+build_dir=`pwd`/build/$PLATFORM
+armor_build_dir=$build_dir/$armor_dir
+kernel_build_dir=$4
+
+#echo "build_dir=$build_dir"
+#echo "armor_build_dir=$armor_build_dir"
+
+###################################################################################
+############################# Setup host environmenta #############################
+###################################################################################
+# Detect and dertermine some environment variables
+LOCALARCH=`uname -m`
+if [ x"$PLATFORM" = x"D01" ]; then
+    TARGETARCH="ARM32"
+else
+    TARGETARCH="ARM64"
+fi
+
+if [ x"$TARGETARCH" = x"ARM32" ]; then
+    cross_gcc=arm-linux-gnueabihf-gcc
+    cross_prefix=arm-linux-gnueabihf
+else
+    cross_gcc=aarch64-linux-gnu-gcc
+    cross_prefix=aarch64-linux-gnu
+fi
+
+###################################################################################
+############################# Build Armor Tools #############################
+###################################################################################
+echo "Building and install Armor Tools ..."
+# copy armor folder to the build directory 
+rm -rf $armor_build_dir
+cp -rf $armor_dir $build_dir
+
+# create armor folders in the rootfs    
+sudo mkdir $ROOTFS/usr/local/armor/
+sudo mkdir $ROOTFS/usr/local/armor/binary
+sudo mkdir $ROOTFS/usr/local/armor/source
+sudo mkdir $ROOTFS/usr/local/armor/test_scripts
+
+pushd $armor_build_dir
+
+cd testing/build_scripts/
+
+# build demidecode
+sh build_dmidecode.sh $cross_gcc
+
+# build kprobes test code
+sh build_kprobes_test.sh $kernel_build_dir
+
+# build lttng kernel module and lttng uspace test code
+sh build_lttng.sh $kernel_build_dir $ROOTFS
+
+cd -
+popd
+
+###################################################################################
+############################# Install Armor Tools #############################
+###################################################################################
+
+echo "Installing package armor ..."
+
+# copy prebuilt binaries to the rootfs
+case $DISTRO in
+    OpenSuse | Fedora)
+        sudo cp $armor_dir/testing/binary/*.rpm   $ROOTFS/usr/local/armor/binary
+        ;;
+    Debian | Ubuntu)
+        sudo cp $armor_dir/testing/binary/*.deb   $ROOTFS/usr/local/armor/binary
+    ;;
+    esac
+sudo cp $armor_dir/testing/binary/crash   $ROOTFS/usr/local/armor/binary
+
+#copy armor test scripts and prebuilt test executables to rootfs
+sudo cp -rf $armor_dir/testing/test_scripts   $ROOTFS/usr/local/armor/
+
+# copy demidecode to rootfs
+sudo cp $armor_build_dir/testing/source/dmidecode/dmidecode $ROOTFS/usr/local/armor/binary
+
+# copy kprobes test binaries to rootfs
+sudo cp $armor_build_dir/testing/source/test_code/kprobes_test_code/kprobe_test  $ROOTFS/usr/local/armor/test_scripts
+sudo cp $armor_build_dir/testing/source/test_code/kprobes_test_code/kprobe_test.ko  $ROOTFS/usr/local/armor/test_scripts
 
 exit 0
