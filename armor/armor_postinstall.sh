e3509325b1b6309b819027caf0c5d16c2f168ac9
shijujose4 <shiju.jose@ymail.com>
2015-11-22 19:00:08 +0000

    re-enabled return error on failure, added new debugging method

--- armor/armor_postinstall.sh
+++ armor/armor_postinstall.sh
@@ -2,123 +2,126 @@
 #author: Shiju Jose
 #date: 05/11/2015
 #description: postinstall scripts for Armor Tools
+armor_post_log="/usr/bin/estuary/postinstall/armor_post_install_log"
+err_flag=0
 
 # Install Armor Tools packages
 # Uncomment below line to install Armor tool's packages.
 INSTALL_ARMOR_TOOLS="YES"
 if [ "$INSTALL_ARMOR_TOOLS" = 'YES' ]; then
     Distribution=`cat /etc/issue| cut -d' ' -f 1`
-    echo "Installing Armor tools packages on $Distribution..." 
-   
+    echo "Installing Armor tools packages on $Distribution..." >> $armor_post_log 
+
     #add path to the perf tool 
     echo "export PATH=/usr/lib/linux-tools-3.19.0-23:\$PATH" >> ~/.bashrc
+    rm $armor_post_log
 
     case "$Distribution" in
-        Ubuntu) 
+        Ubuntu)
             #echo "Ubuntu Distribution"
-            
-            apt-get -y update 
+
+            sudo apt-get -y update
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get -y update failed"
-               #exit 1
+               echo "armor_postinstall: apt-get -y update failed"  >> $armor_post_log
+               err_flag=1
             fi
 
             #Install perf tool
-            apt-get install -y linux-tools-3.19.0-23
+            sudo apt-get install -y linux-tools-3.19.0-23
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y linux-tools-3.19.0-23 failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y linux-tools-3.19.0-23 failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y sysstat  # sar
+            sudo apt-get install -y sysstat  # sar
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y sysstat failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y sysstat failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y gdb 
+            sudo apt-get install -y gdb
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y gdb failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y gdb failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y iptables  
+            sudo apt-get install -y iptables
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y iptables failed"
-               #exit 1
-            fi
- 
-            apt-get install -y dstat  
-            if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y dstat failed"
-               #exit 1
-            fi
-           
-            apt-get install -y iotop  
-            if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y iotop  failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y iptables failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y blktrace  
+            sudo apt-get install -y dstat
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y blktrace failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y dstat failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y nicstat  
+            sudo apt-get install -y iotop
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y nicstat failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y iotop  failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y libconfig9  
+            sudo apt-get install -y blktrace
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y libconfig9 failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y blktrace failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y lldpad  
+            sudo apt-get install -y nicstat
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y lldpad failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y nicstat failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y oprofile  
+            sudo apt-get install -y libconfig9
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y  oprofile failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y libconfig9 failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y latencytop 
+            sudo apt-get install -y lldpad
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y latecytop failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y lldpad failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y systemtap  
+            sudo apt-get install -y oprofile
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y systemtap failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y  oprofile failed" >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y crash 
+            sudo apt-get install -y latencytop
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y  crash failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y latecytop failed"   >> $armor_post_log
+               err_flag=1
             fi
-	   
+
+            sudo apt-get install -y systemtap
+            if [ $? -ne 0 ]; then
+               echo "armor_postinstall: apt-get install -y systemtap failed" >> $armor_post_log
+               err_flag=1
+            fi
+
+            sudo apt-get install -y crash
+            if [ $? -ne 0 ]; then
+               echo "armor_postinstall: apt-get install -y  crash failed"  >> $armor_post_log
+               err_flag=1
+            fi
+
             #install lttng packages
-            apt-get install -y lttng-tools
+            sudo apt-get install -y lttng-tools
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y lttng-tools failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y lttng-tools failed"  >> $armor_post_log
+               err_flag=1
             fi
 
-            apt-get install -y liblttng-ust-dev
+            sudo apt-get install -y liblttng-ust-dev
             if [ $? -ne 0 ]; then
-               echo "armor_postinstall: apt-get install -y liblttng-ust-dev failed"
-               #exit 1
+               echo "armor_postinstall: apt-get install -y liblttng-ust-dev failed" >> $armor_post_log
+               err_flag=1
             fi
 
             # temporary workaround for modules are not getting installed.
@@ -175,14 +178,14 @@ if [ "$INSTALL_ARMOR_TOOLS" = 'YES' ]; then
             dpkg -i /usr/local/armor/binary/dmidecode-1.0-arm64.deb
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dpkg -i /usr/local/armor/binary/dmidecode-1.0-arm64.deb failed"
-               #exit 1
+               err_flag=1
             fi
 
             dpkg -i /usr/local/armor/binary/tiptop-2.3_arm64.deb
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dpkg -i /usr/local/armor/binary/tiptop-2.3_arm64.deb  failed"
-               #exit 1
-            fi
+               err_flag=1
+            fi  
             ;;
            
         Fedora) 
@@ -190,85 +193,85 @@ if [ "$INSTALL_ARMOR_TOOLS" = 'YES' ]; then
             dnf install -y sysstat.aarch64 # sar
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y sysstat.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y dmidecode.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y dmidecode.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y tcpdump.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y tcpdump.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y ethtool.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y ethtool.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
            
             dnf install -y dstat
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y dstat failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y tiptop.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y tiptop.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y iotop.noarch
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y iotop.noarch failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y blktrace.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y blktrace.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y nicstat.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y nicstat.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y lldpad.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y lldpad.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y oprofile.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y oprofile.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y latencytop.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y latencytop.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y systemtap.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y systemtap.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             dnf install -y crash.aarch64
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: dnf install -y crash.aarch64 failed"
-               exit 1
+               err_flag=1
             fi
 
             # install prebuilt rpm packages 
@@ -279,95 +282,100 @@ if [ "$INSTALL_ARMOR_TOOLS" = 'YES' ]; then
             zypper install -y ltrace
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y ltrace failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y pcp-import-iostat2pcp # for sar, iostat etc.
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y pcp-import-iostat2pcp failed"
-               exit 1
+               err_flag=1
             fi
             
             zypper install -y dmidecode
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y dmidecode failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y strace
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y strace failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y net-tools-deprecated
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y net-tools-deprecated failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y net-tools
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y net-tools failed"
-               exit 1
+               err_flag=1
             fi
             
             zypper install -y dstat 
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y dstat failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y procps
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y procps failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y iotop
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y iotop failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y blktrace
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y blktrace failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y oprofile
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y oprofile failed"
-               exit 1
+               err_flag=1
             fi
 
             zypper install -y systemtap
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: zypper install -y systemtap failed"
-               exit 1
+               err_flag=1
             fi
             
             # install prebuilt rpm packages 
             rpm -i /usr/local/armor/binary/lldpad-1.0.1-0.aarch64.rpm 
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: rpm -i /usr/local/armor/binary/lldpad-1.0.1-0.aarch64.rpm failed"
-               exit 1
+               err_flag=1
             fi
 
             rpm -i /usr/local/armor/binary/nicstat-1.95-0.aarch64.rpm 
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: rpm -i /usr/local/armor/binary/nicstat-1.95-0.aarch64.rpm failed"
-               exit 1
+               err_flag=1
             fi
 
             rpm -i /usr/local/armor/binary/tiptop-2.3-0.aarch64.rpm
             if [ $? -ne 0 ]; then
                echo "armor_postinstall: rpm -i /usr/local/armor/binary/tiptop-2.3-0.aarch64.rpm failed"
-               exit 1
+               err_flag=1
             fi
             ;;
         esac
-        echo "Finished installation of Armor tools packages" 
+
+        if [ $err_flag -eq 1 ]; then
+           echo "Finished installation of Armor tools packages with Errors" >> $armor_post_log
+           exit 1
+        fi 
+        echo "Finished installation of Armor tools packages Successfully" >> $armor_post_log
     fi
 
