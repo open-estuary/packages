32d0e4b47931307205e30e6d52c446acf27c65bb
bobsense <liubowen2@huawei.com>
2016-01-18 10:13:36 +0800

    update build.sh version 3
    modify the path problem

--- mysql/build.sh
+++ mysql/build.sh
@@ -30,8 +30,8 @@
 #    fi
 #######
     echo "Installing mysql on $Distribution..."
-    cd percona-server-5.6.22-72.0
-    cd BUILD
+    cd ./percona-server-5.6.22-72.0
+    cd ./BUILD
     sh autorun.sh
     cd ..
     cmake -DCMAKE_INSTALL_PREFIX=/u01/my3306 -DMYSQL_DATADIR=/u01/my3306/data -DMYSQL_USER=mysql -DSYSCONFDIR=/etc  -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_INNOBASE_STORAGE_ENGINE=1-DWITH_MEMORY_STORAGE_ENGINE=1 -DMYSQL_UNIX_ADDR=/u01/my3306/run/mysql.sock -DMYSQL_TCP_PORT=3306 -DENABLED_LOCAL_INFILE=1  -DWITH_PARTITION_STORAGE_ENGINE=1 -DEXTRA_CHARSETS=all -DDEFAULT_CHARSET=utf8  -DDEFAULT_COLLATION=utf8_general_ci
@@ -43,7 +43,7 @@
     make install
     groupadd mysql
     useradd –g mysql mysql
-    cp my-sigle.cnf  /etc/my.cnf
+    cp ./my-sigle.cnf  /etc/my.cnf
     mkdir /u01/mysql
     cp –rf /u01/my3306/share /u01/mysql
     mkdir /u01/my3306/tmp
