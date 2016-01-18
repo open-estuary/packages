948e82b43a856fb70cc3056d6ca5b8575f76536d
bobsense <liubowen2@huawei.com>
2016-01-18 10:02:37 +0800

    update build.sh version 2
    follow the title

--- mysql/build.sh
+++ mysql/build.sh
@@ -3,31 +3,31 @@
 #date: 01/15/2016
 # Install mysql on ubuntu
 
-Distribution=`cat /etc/issue| cut -d' ' -f 1`
-echo $Distribution
-ResolvConfFile=/etc/resolv.conf
+#Distribution=`cat /etc/issue| cut -d' ' -f 1`
+#echo $Distribution
+#ResolvConfFile=/etc/resolv.conf
 
-case "$Distribution" in
-  Ubuntu)
+#case "$Distribution" in
+#  Ubuntu)
     
 ########
-    grep nameserver "$ResolvConfFile"
-    if [ "$?" == 1 ]; then
-      echo "nameserver 8.8.8.8" >> "$ResolvConfFile"
-      echo "--------configure $ResolvConfFile------"
-      source /etc/resolv.conf
-    fi
+#    grep nameserver "$ResolvConfFile"
+#    if [ "$?" == 1 ]; then
+#      echo "nameserver 8.8.8.8" >> "$ResolvConfFile"
+#      echo "--------configure $ResolvConfFile------"
+#      source /etc/resolv.conf
+#    fi
 ########
-    apt-get install cmake
-    if [ "$?" == 1 ] ; then
-      echo "apt-get install cmake failed..."
-      exit 1
-    fi
-    apt-get install g++
-    if [ "$?" == 1 ] ; then
-      echo "apt-get install g++ failed..."
-      exit 1
-    fi
+#    apt-get install cmake
+#    if [ "$?" == 1 ] ; then
+#      echo "apt-get install cmake failed..."
+#      exit 1
+#    fi
+#    apt-get install g++
+#    if [ "$?" == 1 ] ; then
+#      echo "apt-get install g++ failed..."
+#      exit 1
+#    fi
 #######
     echo "Installing mysql on $Distribution..."
     cd percona-server-5.6.22-72.0
@@ -56,13 +56,13 @@ case "$Distribution" in
     /u01/my3306/bin/mysqld_safe --defaults-file=/etc/my.cnf --basedir=/u01/my3306 --datadir=/u01/my3306/data &
     /u01/my3306/bin/mysql –uroot
     SET PASSWORD = PASSWORD('123456');
-	  UPDATE mysql.user SET password=PASSWORD('123456') WHERE User='mysql';
-	  GRANT ALL PRIVILEGES ON *.* TO mysql@localhost IDENTIFIED BY '123456' WITH GRANT OPTION;
+    UPDATE mysql.user SET password=PASSWORD('123456') WHERE User='mysql';
+    GRANT ALL PRIVILEGES ON *.* TO mysql@localhost IDENTIFIED BY '123456' WITH GRANT OPTION;
     GRANT ALL PRIVILEGES ON *.* TO mysql@"%" IDENTIFIED BY '123456' WITH GRANT OPTION;
     GRANT ALL PRIVILEGES ON *.* TO root@localhost IDENTIFIED BY '123456' WITH GRANT OPTION;
     GRANT ALL PRIVILEGES ON *.* TO root@"%" IDENTIFIED BY '123456' WITH GRANT OPTION;
     exit
-  ;;
-  *)
-  esac
+#  ;;
+#  *)
+#  esac
 echo "Finished installation of mysql"
