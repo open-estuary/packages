e66c7630ec026341740ace865febbb3ad9959b7f
bobsense <liubowen2@huawei.com>
2016-02-14 17:15:36 +0800

    add different distros

--- mysql/mysql_postinstall.sh
+++ mysql/mysql_postinstall.sh
@@ -1,14 +1,40 @@
-    groupadd mysql
-    useradd -g mysql mysql
-    cd ..
-    mkdir /u01/mysql
-    cp -rf /u01/my3306/share /u01/mysql
-    mkdir /u01/my3306/tmp
-    mkdir /u01/my3306/log
-    mkdir /u01/my3306/run
-    cd /u01
-    chown -R mysql.root ./
-    cd /u01/my3306
-    scripts/mysql_install_db --basedir=/u01/my3306 --datadir=/u01/my3306/data --user=mysql
-    /u01/my3306/bin/mysqld_safe --defaults-file=/etc/my.cnf --basedir=/u01/my3306 --datadir=/u01/my3306/data &
-    echo "mysql install finished"
+#!/bin/bash
+#author: Bowen Liu
+#date: 14/02/2016
+#description: postinstall scripts for mysql
+
+Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 1`
+# Temp fix for OpenSuse distribution as the format of /etc/issue in OpenSuse is different
+Distribution1=`sed -n 1p /etc/issue| cut -d' ' -f 3`
+if [ "$Distribution1" = 'openSUSE' ]; then
+    Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 3`
+fi
+
+echo "Installing Mysql on $Distribution..."
+    
+case "$Distribution" in
+    Fedora)
+        ;;
+    OpenSuse)
+        ;;
+    Ubuntu)
+        ;;
+    Debian)
+    ;;
+    esac
+    
+groupadd mysql
+useradd -g mysql mysql
+cd ..
+mkdir /u01/mysql
+cp -rf /u01/my3306/share /u01/mysql
+mkdir /u01/my3306/tmp
+mkdir /u01/my3306/log
+mkdir /u01/my3306/run
+cd /u01
+chown -R mysql.root ./
+cd /u01/my3306
+scripts/mysql_install_db --basedir=/u01/my3306 --datadir=/u01/my3306/data --user=mysql
+/u01/my3306/bin/mysqld_safe --defaults-file=/etc/my.cnf --basedir=/u01/my3306 --datadir=/u01/my3306/data &
+
+echo "mysql install finished successfully"
