e0f6045c847ca8a46046038a15e74c34f56c3ae3
bobsense <liubowen2@huawei.com>
2016-02-14 10:25:51 +0800

    update build.sh 16-2.14

--- mysql/build.sh
+++ mysql/build.sh
@@ -44,7 +44,7 @@ Y
 EOF
     echo "finish install cmake"
     cmake -DCMAKE_INSTALL_PREFIX=/u01/my3306 -DMYSQL_DATADIR=/u01/my3306/data -DMYSQL_USER=mysql -DSYSCONFDIR=/etc -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_MEMORY_STORAGE_ENGINE=1 -DMYSQL_UNIX_ADDR=/u01/my3306/run/mysql.sock -DMYSQL_TCP_PORT=3306 -DENABLED_LOCAL_INFILE=1 -DWITH_PARTITION_STORAGE_ENGINE=1 -DEXTRA_CHARSETS=all -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci
-    make -j16 $cross_gcc
+    make -j16
     sudo make DESTDIR=$ROOTFS install
     sudo cp $current_dir/my-sigle.cnf  $ROOTFS/etc/my.cnf
     echo "mysql make install finished"
