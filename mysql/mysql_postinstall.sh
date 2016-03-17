7fc9a57e5d73fba6b156ca4303eee947ca964008
bobsense <liubowen2@huawei.com>
2016-03-17 21:05:06 +0800

    fix mysql_postinstall.sh bug and add mysql.service run automatically

--- mysql/mysql_postinstall.sh
+++ mysql/mysql_postinstall.sh
@@ -23,18 +23,25 @@ case "$Distribution" in
     ;;
     esac
     
-groupadd mysql
-useradd -g mysql mysql
-cd ..
-mkdir /u01/mysql
-cp -rf /u01/my3306/share /u01/mysql
-mkdir /u01/my3306/tmp
-mkdir /u01/my3306/log
-mkdir /u01/my3306/run
-cd /u01
-chown -R mysql.root ./
-cd /u01/my3306
-scripts/mysql_install_db --basedir=/u01/my3306 --datadir=/u01/my3306/data --user=mysql
-/u01/my3306/bin/mysqld_safe --defaults-file=/etc/my.cnf --basedir=/u01/my3306 --datadir=/u01/my3306/data &
-
-echo "mysql install finished successfully"
+mysql_path="/u01"
+if [ -d "$mysql_path" ]; then
+    groupadd mysql
+    useradd -g mysql mysql
+    cd ..
+    mkdir /u01/mysql
+    cp -rf /u01/my3306/share /u01/mysql
+    mkdir /u01/my3306/tmp
+    mkdir /u01/my3306/log
+    mkdir /u01/my3306/run
+    cd /u01
+    chown -R mysql.root ./
+    cd /u01/my3306
+    scripts/mysql_install_db --basedir=/u01/my3306 --datadir=/u01/my3306/data --user=mysql
+    /u01/my3306/bin/mysqld_safe --defaults-file=/etc/my.cnf --basedir=/u01/my3306 --datadir=/u01/my3306/data &
+    export PATH=/u01/my3306/bin:$PATH
+    echo "export PATH=/u01/my3306/bin:$PATH">>/etc/profile
+    systemctl enable mysql
+    echo "mysql install finished successfully"
+else
+    echo "WARNING:mysql should be made from aarch64!"
+fi
