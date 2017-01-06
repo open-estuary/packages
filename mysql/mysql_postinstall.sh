8c45c8b737614f4c75b14fb9f15e8655595fe604
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-01-06 16:14:03 +0800

    update mysql install script

--- mysql/mysql_postinstall.sh
+++ mysql/mysql_postinstall.sh
@@ -3,21 +3,12 @@
 #date: 01/05/2017
 #description: postinstall scripts for mysql
 
-Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 1`
-# Temp fix for OpenSuse distribution as the format of /etc/issue in OpenSuse is different
-Distribution1=`sed -n 1p /etc/issue| cut -d' ' -f 3`
-if [ "$Distribution1" = 'openSUSE' ]; then
-    Distribution=`sed -n 1p /etc/issue| cut -d' ' -f 3`
-fi
-
-echo "Installing Mysql on $Distribution..."
-
 #Step 1: Install docker firstly
-if [ -z "$(which docker > /dev/null)" ]; then
+if [ -z "$(which docker 2>/dev/null)" ]; then
     INSTALL_CMD=""
-    if [ ! -z "$(which yum > /dev/null)" ] ; then
+    if [ ! -z "$(which yum 2>/dev/null)" ] ; then
         INSTALL_CMD="yum"
-    elif [ ! -z "$(which apt-get > /dev/null)" ] ; then
+    elif [ ! -z "$(which apt-get 2>/dev/null)" ] ; then
         INSTALL_CMD="apt-get"
     else 
         echo "Not know how to install docker ..."
@@ -32,7 +23,9 @@ if [ -z "$(ps -aux | grep docker | grep -v grep)" ] ; then
 fi
 
 #Step 3: Try to pull latest mysql docker image
-if [ -z "$(docker images | grep openestuary/mysql | grep -v grep)"] ; then
+if [ -z "$(docker images | grep 'openestuary/mysql' | grep -v grep)" ]  ; then
     docker pull openestuary/mysql
+else 
+    echo "MySql docker image has been pulled"
 fi
 
