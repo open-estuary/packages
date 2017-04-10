fcc722fe154f4db7107a6bdea3c17d865a72e665
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-04-10 16:40:31 +0800

    add command to install docker

--- docker_apps/lamp/setup.sh
+++ docker_apps/lamp/setup.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-#Step 1: Start docker service
+#step 1: Install docker firstly
+if [ -z "$(which docker 2>/dev/null)" ]; then
+    INSTALL_CMD=""
+    if [ ! -z "$(which yum 2>/dev/null)" ] ; then
+        INSTALL_CMD="yum"
+    elif [ ! -z "$(which apt-get 2>/dev/null)" ] ; then
+        INSTALL_CMD="apt-get"
+    else 
+        echo "Not know how to install docker ..."
+        exit 0
+    fi
+    ${INSTALL_CMD} install -y -q docker docker.io
+fi
+
+#Step 2: Start docker service
 if [ -z "$(ps -aux | grep docker | grep -v grep)" ] ; then
         service docker start
 fi
 
-#Step 2: Download docker images
+#Step 3: Download docker images
 docker pull openestuary/apache
 docker pull openestuary/mysql
 
