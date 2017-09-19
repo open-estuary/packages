62396c4c3aed1a6b2ec2f44ed71fa171935c533b
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-19 10:44:26 +0800

    add git ignore files

--- solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
+++ solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
+if [ -z "${2}" ] ; then
+    echo "Usage: <solr server ip> <query_file> [<solr server port>]"
+    exit 0
+fi
+
+if [ ! -f "${2}" ] ; then
+    echo "The file:${2} does not exist"
+    exit 0
+fi
+
 if [ -z "$(which jmeter 2>/dev/null)" ] ; then
     JMETER="/usr/local/jmeter/apache-jmeter-3.2/bin/jmeter"
 else 
     JMETER="jmeter"
 fi
 
-BENCHMARK_JMX="./SolrQueryTest.jmx"
-HOST="192.168.1.86"
-PORT="8983"
+CUR_DIR="$(cd `dirname $0`; pwd)"
+BENCHMARK_JMX="${CUR_DIR}/solr_benchmark.jmx"
+
+HOST="${1}"
+PORT=${3:-8983}
+
 SERVERPATH="/solr/e-commerce"
-QUERYFILE="./product_query"
+QUERYFILE="${2}"
 
 ${JMETER} -n -t ${BENCHMARK_JMX} -JserverName=${HOST} -JserverPort=${PORT} -JserverPath=${SERVERPATH} -JqueryFile="${QUERYFILE}"
