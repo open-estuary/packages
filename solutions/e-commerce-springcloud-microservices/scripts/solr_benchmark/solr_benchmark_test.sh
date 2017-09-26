d3c78ff26df082712fcf01ed10ab032f838de05a
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-26 13:44:43 +0800

    update solr benchmark test

--- solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
+++ solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_benchmark_test.sh
@@ -5,7 +5,7 @@
 #
 
 if [ -z "${2}" ] ; then
-    echo "Usage: <solr server ip> <query_file> <solr server port> <number_of_user> <time_in_sec> <remote_agent_hosts>"
+    echo "Usage: <solr server ip> <solr server port> <number_of_user> <time_in_sec> <remote_agent_hosts>"
     exit 0
 fi
 
@@ -16,7 +16,7 @@ echo "                                  ********                                
 #Jmeter client which triggers Jmeter servers to start tests and manage test results. 
 LOCAL_HOST="192.168.11.247"
 #Jmeter server (or agents) which perform real test works
-REMOTE_HOST=${6:-"192.168.11.247,192.168.11.246"}
+REMOTE_HOST=${5:-"192.168.11.247,192.168.11.246"}
 
 if [ -z "$(which jmeter 2>/dev/null)" ] ; then
     JMETER="/opt/jmeter/bin/jmeter"
@@ -28,13 +28,15 @@ CUR_DIR="$(cd `dirname $0`; pwd)"
 BENCHMARK_JMX="${CUR_DIR}/solr_benchmark.jmx"
 
 HOST="${1}"
-PORT=${3:-8983}
+PORT=${2:-8983}
 
-USER_NUM=${4:-100}
-DUR_TIME_INSEC=${5:-240}
+USER_NUM=${3:-100}
+DUR_TIME_INSEC=${4:-240}
 
 SERVERPATH="/solr/e-commerce"
-QUERYFILE="${2}"
+#By default, the solr ansible scripts will install solr_query file under /home/estuaryapp/solr_benchmark directory
+#Please also make sure this file exists on all remote_hosts servers.
+QUERYFILE="/home/estuaryapp/solr_benchmark/solr_query"
 
 CUR_DIR="$(cd `dirname $0`; pwd)"
 if [ -f "${CUR_DIR}/solr_benchmark_result.jtl" ] ; then
@@ -43,6 +45,6 @@ if [ -f "${CUR_DIR}/solr_benchmark_result.jtl" ] ; then
     rm ${CUR_DIR}/jmeter.log
 fi
 
-echo "Perform New Solr Test(LOCAL_HOST:${LOAL_HOST}, REMOTE_HOST:${REMOTE_HOST}, Target Server:${HOST}, Target Port:${PORT}, NumberofUser:${USER_NUM}, TestTimeInSecs:${DUR_TIME_INSEC}"
+echo "Perform New Solr Test(LOCAL_HOST:${LOCAL_HOST}, REMOTE_HOST:${REMOTE_HOST}, Target Server:${HOST}, Target Port:${PORT}, NumberofUser:${USER_NUM}, TestTimeInSecs:${DUR_TIME_INSEC}, QUERYFILE:${QUERYFILE}"
 taskset -c 2-63 ${JMETER} -n -t ${BENCHMARK_JMX} -Djava.rmi.server.hostname=${LOCAL_HOST} -GserverName=${HOST} -GserverPort=${PORT} -GserverPath=${SERVERPATH} -GqueryFile="${QUERYFILE}" -GnoOfUsers ${USER_NUM} -GdurationInSecs ${DUR_TIME_INSEC} -l ${CUR_DIR}/solr_benchmark_result.jtl -o ${CUR_DIR}/solr_benchmark_report -e -R"${REMOTE_HOST}"
 
