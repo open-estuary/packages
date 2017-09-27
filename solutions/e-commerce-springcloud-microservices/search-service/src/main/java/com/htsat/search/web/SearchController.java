619d0bc8d406f62c11ab019e31475ea823480faf
zhouxingchen1993 <zhouxingchen93@163.com>
2017-09-27 21:15:51 +0800

    update search

--- solutions/e-commerce-springcloud-microservices/search-service/src/main/java/com/htsat/search/web/SearchController.java
+++ solutions/e-commerce-springcloud-microservices/search-service/src/main/java/com/htsat/search/web/SearchController.java
@@ -4,6 +4,11 @@ import com.htsat.search.dto.StatusDTO;
 import com.htsat.search.enums.ExcuteStatusEnum;
 import com.htsat.search.service.ISearchService;
 import org.apache.log4j.Logger;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.apache.solr.common.SolrDocumentList;
+import org.apache.solr.common.StringUtils;
+import org.apache.solr.common.params.ModifiableSolrParams;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,6 +30,49 @@ public class SearchController {
         logger.info("query : " + query + "   page_size : " + page_size + "   page_num : "+ page_num + "   sort : "+ sort +"   search : " + order);
         logger.info("call success!!!");
         return status;
+    }
+
+
+
+    @Autowired
+    private SolrClient client;
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET)
+    public void searchBySolr(@RequestParam(value = "query", required = false) String query, @RequestParam(value = "page_size", required = false) Integer page_size,
+                             @RequestParam(value = "page_num", required = false) Integer page_num, @RequestParam(value = "sort", required = false) String sort,
+                             @RequestParam(value = "order", required = false) String order) {
+
+        logger.info("query : " + query + "     " + "page_size : " + page_size + "     " + "page_num : " + page_num + "     " + "sort : " + sort + "     " + "order : " + order);
+
+        ModifiableSolrParams params =new ModifiableSolrParams();
+        if (!StringUtils.isEmpty(query))
+            params.add("q",query);
+
+        params.add("wt","json");
+
+        int start;
+        if(page_num != null && page_num > 0 && page_size != null && page_size >= 0) {
+            start = page_size * (page_num - 1) ;
+            params.add("start", (start + ""));
+        }
+
+        if(page_size != null && page_size >= 0) {
+            params.add("rows",(page_size + ""));
+        }
+
+        if (!StringUtils.isEmpty(sort))
+            params.add("sort", sort);
+
+        QueryResponse response=null;
+        try{
+            response=client.query(params);
+            SolrDocumentList results = response.getResults();
+            logger.info("results : " + results.toString());
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        System.out.println(response.toString());
+        logger.info(response.toString());
     }
 
 }
--- solutions/e-commerce-springcloud-microservices/search-service/src/test/java/com/htsat/search/SearchApplicationTests.java
+++ solutions/e-commerce-springcloud-microservices/search-service/src/test/java/com/htsat/search/SearchApplicationTests.java
@@ -5,6 +5,13 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SearchApplicationTests {
