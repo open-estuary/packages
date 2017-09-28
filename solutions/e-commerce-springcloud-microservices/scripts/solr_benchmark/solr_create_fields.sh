7979a348677f7031e3f31806f213fe240498c787
sjtuhjh@hotmail.com <sjtuhjh@hotmail.com>
2017-09-28 11:05:59 +0800

    update cache size

--- solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_create_fields.sh
+++ solutions/e-commerce-springcloud-microservices/scripts/solr_benchmark/solr_create_fields.sh
@@ -19,19 +19,19 @@ sudo -u ${user} ${solr_cmd} create -c "${core}" -p "${port}"
 
 ######### update e-commerce cache_size ########################################################
 curl -X POST -H 'Content-type:application/json' --data-binary \
-      '{"set-property": { "query.filterCache.size":10240000, "query.filterCache.initialSize":10240000, "query.filterCache.autowarmCount":4096}}' \
+      '{"set-property": { "query.filterCache.size":1024000, "query.filterCache.initialSize":102400, "query.filterCache.autowarmCount":4096}}' \
       http://${host}:${port}/solr/${core}/config
 
 curl -X POST -H 'Content-type:application/json' --data-binary \
-      '{"set-property": { "query.queryResultCache.size":10240000, "query.queryResultCache.initialSize":10240000, "query.queryResultCache.autowarmCount":4096}}' \
+      '{"set-property": { "query.queryResultCache.size":1024000, "query.queryResultCache.initialSize":102400, "query.queryResultCache.autowarmCount":4096}}' \
       http://${host}:${port}/solr/${core}/config
 
 curl -X POST -H 'Content-type:application/json' --data-binary \
-      '{"set-property": { "query.documentCache.size":10240000, "query.documentCache.initialSize":10240000, "query.documentCache.autowarmCount":4096}}' \
+      '{"set-property": { "query.documentCache.size":1024000, "query.documentCache.initialSize":102400, "query.documentCache.autowarmCount":4096}}' \
       http://${host}:${port}/solr/${core}/config
 
 curl -X POST -H 'Content-type:application/json' --data-binary \
-      '{"set-property": { "query.fieldValueCache.size":10240000, "query.fieldValueCache.initialSize":10240000, "query.fieldValueCache.autowarmCount":4096}}' \
+      '{"set-property": { "query.fieldValueCache.size":1024000, "query.fieldValueCache.initialSize":102400, "query.fieldValueCache.autowarmCount":4096}}' \
       http://${host}:${port}/solr/${core}/config
 
 ######### create chinese field type ############################################################
